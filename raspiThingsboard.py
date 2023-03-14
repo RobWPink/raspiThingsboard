e4cbe87d2c64164c1201e4157aa244677defca39
RobWPink <robert.pink@oneh2.com>
2023-03-14 12:49:19 -0400

    added arguments

--- raspiThingsboard.py
+++ raspiThingsboard.py
@@ -8,14 +8,24 @@ host = '3.142.96.59'
 port = 1883
 telemetry = 'v1/devices/me/telemetry'
 attributes = 'v1/devices/me/attributes'
-clientId = "reformer2"
-username = 'v1Tu9MBLtUhNbs2ygGKf'
-password = 'test1234'
-zeroOut = '[{"time":0},{"tt511":0},{"tt512":0},{"burnerTemp":0},{"tt513":0},{"tt514":0},{"tt407":0},{"tt408":0},{"tt410":0},{"tt411":0},{"tt430":0},{"tt441":0},{"tt442":0},{"tt443":0},{"tt444":0},{"tt445":0},{"tt446":0},{"tt447":0},{"tt448":0},{"tt449":0},{"tubeMean":0},{"tubeMax":0},{"tt142":0},{"tt301":0},{"tt303":0},{"tt306":0},{"tt313":0},{"tt319":0},{"bmmAlarm":0},{"bmmProof":0},{"estop":0},{"greenButton":0},{"greenPilot":0},{"amberButton":0},{"amberPilot":0},{"psh":0},{"psl":0},{"zsl":0},{"bmmRun":0},{"xv501":0},{"xv217":0},{"xv474":0},{"xv1100":0},{"xv122":0},{"twv308":0},{"twv310":0},{"fcv134FeedBack":0},{"bl508FeedBack":0},{"pmp204FeedBack":0},{"fcv549FeedBack":0},{"pt654":0},{"pt304":0},{"pt420":0},{"pt383":0},{"pt318":0},{"ft132":0},{"ft219":0},{"bl508":0},{"fcv134":0},{"pmp204":0},{"fcv141":0},{"fcv205":0},{"fcv549":0}]'
+
+if sys.argv[1]:
+  clientId = sys.argv[1]
+else:
+  clientId = 'unknownID'
+  
+if sys.argv[2]:
+  username = sys.argv[2]
+else:
+  username = 'v1Tu9MBLtUhNbs2ygGKf'
+
+if sys.argv[3]:
+  password = sys.argv[3]
+else:
+  password = 'test1234'
 
 #[time,tt511,tt512,burnerTemp,tt513,tt514,tt407,tt408,tt410,tt411,tt430,tt441,tt442,tt443,tt444,tt445,tt446,tt447,tt448,tt449,tubeMean,tubeMax,tt142,tt301,tt303,tt306,tt313,tt319,bmmAlarm,bmmProof,estop,greenButton,greenPilot,amberButton,amberPilot,psh,psl,zsl,bmmRun,xv501,xv217,xv474,xv1100,xv122,twv308,twv310,fcv134FeedBack,bl508FeedBack,pmp204FeedBack,fcv549FeedBack,pt654,pt304,pt420,pt383,pt318,ft132,ft219,bl508,fcv134,pmp204,fcv141,fcv205,fcv549]
 prev_msg = ''
-zero = False
 sendAll = True
 flushData = False
 allData = {
@@ -104,7 +114,6 @@ class sendDataProgram:
     def publish(client,topic):
       global allData
       global flushData
-      global zero
       global all
       while True:
         time.sleep(0.5)
@@ -124,39 +133,30 @@ class sendDataProgram:
           try:
             data = [float(i) for i in parsed]
             if len(data) == 63:
-              i = 0
-              if zero:
-                print(zeroOut)
-                result = client.publish(topic, zeroOut)
-                status = result[0]
-                if not status == 0:
-                  print(f"Failed to send message to topic {topic}")
-                msg = ''
-                zero = False
-              else:  
-                msg = '['
-                for key in allData:
-                  if flushData:
-                    ser.flushInput()
-                    ser.flushOutput()
-                    time.sleep(0.1)
-                    flushData = False
-                    break
-                  if not allData[key] == data[i] or sendAll:
-                    msg = msg + '{"'+key+'":'+str(allData[key])+'},' 
-                    allData[key] = data[i]
-                    if i % 10 == 0 or i == 62:
-                      msg = msg[:-1]
-                      msg = msg + ']'
-                      print(msg)
-                      result = client.publish(topic, msg)
-                      status = result[0]
-                      if not status == 0:
-                        print(f"Failed to send message to topic {topic}")
-                        msg = '['
-                      else:
-                        msg = '['
-                        time.sleep(0.25)
+              i = 0 
+              msg = '['
+              for key in allData:
+                if flushData:
+                  ser.flushInput()
+                  ser.flushOutput()
+                  time.sleep(0.1)
+                  flushData = False
+                  break
+                if not allData[key] == data[i] or sendAll:
+                  msg = msg + '{"'+key+'":'+str(allData[key])+'},' 
+                  allData[key] = data[i]
+                  if i % 10 == 0 or i == 62:
+                    msg = msg[:-1]
+                    msg = msg + ']'
+                    print(msg)
+                    result = client.publish(topic, msg)
+                    status = result[0]
+                    if not status == 0:
+                      print(f"Failed to send message to topic {topic}")
+                      msg = '['
+                    else:
+                      msg = '['
+                      time.sleep(0.25)
                   i = i + 1 #for loop int increment
                 ser.reset_input_buffer()
               
@@ -181,7 +181,6 @@ class receiveDataProgram:
     
     def subscribe(client: mqtt_client):
       def on_message(client, userdata, msg):
-        global zero
         global sendAll
         global flushData
         try:
@@ -200,10 +199,7 @@ class receiveDataProgram:
             else:
               output = name +" "+ number
           elif "legacy" in received and not "deleted" in received:
-            if "zero" in received:
-              zero = True
-              output = ''
-            elif "all" in received:
+            if "all" in received:
               sendAll = not sendAll
               output = ''
             else:
